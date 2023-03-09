87a550df317d209f879c3d1bccc2a5ab415ffe29
anmol0700k <117492721+anmol0700k@users.noreply.github.com>
2023-03-09 10:08:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Greymattersbot/EvaMaria /EvaMaria
+  git clone https://github.com/anmol0700k/Advance-EvaMaria1 /Advance-EvaMaria1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Advance-EvaMaria1
 fi
-cd /EvaMaria
+cd /Advance-EvaMaria1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
