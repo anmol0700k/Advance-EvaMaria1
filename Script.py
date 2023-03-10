d8e7c38cf05930fbfa97b338f5c862fd53c35193
anmol0700k <117492721+anmol0700k@users.noreply.github.com>
2023-03-10 13:12:05 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -19,7 +19,7 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Ayush_Sharma_15>ᵀᵉᵃᵐ༒Legend࿐</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/IND07Ayush>ᴀʏᴜꜱʜ ꜱʜᴀʀᴍᴀ</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
@@ -27,9 +27,9 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - 𝗧𝗵𝗶𝘀 𝗜𝘀 𝗡𝗼𝘁 𝗔 𝗢𝗽𝗲𝗻 𝗦𝗼𝘂𝗿𝗰𝗲 𝗣𝗿𝗼𝗷𝗲𝗰𝘁. 
-- Source -  <a href=https://t.me/+zSBT7Ednrf9kN2Y1>ᵀᵉᵃᵐ༒Legend࿐</a>
+- Source -  <a href=https://t.me/IND07Ayush>ᴀʏᴜꜱʜ ꜱʜᴀʀᴍᴀ</a>
 <b>DEVS:</b>
-- <a href=https://t.me/+zSBT7Ednrf9kN2Y1>ᵀᵉᵃᵐ༒Legend࿐</a>"""
+- <a href=https://t.me/IND07Ayush>ᴀʏᴜꜱʜ ꜱʜᴀʀᴍᴀ</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
     
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
